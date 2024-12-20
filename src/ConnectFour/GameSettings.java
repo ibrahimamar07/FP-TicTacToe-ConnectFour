066e3c5f510fc14166d4449c1491a1ab92f0f820
ibrahimamar07 <ibrahimamaralfanani07@gmail.com>
2024-12-20 21:26:24 +0700

    fix

--- src/ConnectFour/GameSettings.java
+++ src/ConnectFour/GameSettings.java
@@ -1,69 +1,12 @@
-//package ConnectFour;
-//
-//public class GameSettings {
-//    private static int difficulty = 1; // 1=Easy, 2=Medium, 3=Hard
-//    private static String theme = "Classic"; // Classic, Space, Ocean
-//    private static boolean soundEnabled = true;
-//    private static int playerScore = 0;
-//    private static int aiScore = 0;
-//
-//    public static void setDifficulty(int level) {
-//        difficulty = level;
-//    }
-//
-//    public static int getDifficulty() {
-//        return difficulty;
-//    }
-//
-//    public static void setTheme(String newTheme) {
-//        theme = newTheme;
-//    }
-//
-//    public static String getTheme() {
-//        return theme;
-//    }
-//
-//    public static void toggleSound() {
-//        soundEnabled = !soundEnabled;
-//    }
-//
-//    public static boolean isSoundEnabled() {
-//        return soundEnabled;
-//    }
-//
-//    public static void updateScore(boolean playerWon) {
-//        if (playerWon) {
-//            playerScore++;
-//        } else {
-//            aiScore++;
-//        }
-//    }
-//
-//    public static int getPlayerScore() {
-//        return playerScore;
-//    }
-//
-//    public static int getAIScore() {
-//        return aiScore;
-//    }
-//
-//    public static void resetScores() {
-//        playerScore = 0;
-//        aiScore = 0;
-//    }
-//}
-//
-//
-//
-
-
-
-
-
-
-
-
-
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group 15
+ * 1 - 5026231195- ibrahim amar alfanani
+ * 2 - 5026231219- ghifari rabbani A
+ * 3 - 5026231180 - favian astama
+ */
 package ConnectFour;
 
 import java.awt.*;
